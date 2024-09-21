559a6802d7dc32f3c0f06a6803516a28c6d87b7d
Kishor <aaaaarrrgghhh@yahoo.com>
2024-09-21 08:18:35 +0800

    correct cluster auth name

--- main.tf
+++ main.tf
@@ -477,7 +477,7 @@ data "aws_caller_identity" "current" {}
 
 # Fetch the authentication token for EKS
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 # Correct Kubernetes provider configuration
