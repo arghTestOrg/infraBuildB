b8a68dae30de197cbeca67987156d9ea56c556a0
Kishor <aaaaarrrgghhh@yahoo.com>
2024-09-22 12:07:45 +0800

    added dns hostnames to vpc

--- main.tf
+++ main.tf
@@ -9,7 +9,8 @@ data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "prod_vpc" {
   cidr_block = "10.1.0.0/16"
-
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
     name = var.aws_vpc_name
   }
